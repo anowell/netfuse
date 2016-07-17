85ad57402f4d09fc26afabac45abb38bf76b941f
Anthony Nowell <anowell@gmail.com>
2016-07-16 23:53:16 -0600

    Initial docs

---
+++ implementors/fuse/trait.Filesystem.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["netfuse"] = ["impl&lt;NFS:&nbsp;<a class='trait' href='netfuse/trait.NetworkFilesystem.html' title='netfuse::NetworkFilesystem'>NetworkFilesystem</a>&gt; Filesystem for <a class='struct' href='netfuse/struct.NetFuse.html' title='netfuse::NetFuse'>NetFuse</a>&lt;NFS&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
